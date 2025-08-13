5d4c9ebf948dfc586916e2e08fba65cc189c5bd0
Javier Fransiscus <pier9402@gmail.com>
2025-08-13 10:51:52 +0800

    Initial Commit

---
+++ Localization/LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationApp.swift
+//  Localization
+//
+//  Created by Javier Fransiscus on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
